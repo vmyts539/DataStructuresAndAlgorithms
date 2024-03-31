c671f55084f0d366212d05bdb0568398ce63be76
Vasyl Mytskaniuk <vasul2537125@gmail.com>
2024-03-31 11:06:02 +0300

    quicksort, binary tree

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,38 +1,148 @@
 type Node<T> = {
-  value: T,
-  prev?: Node<T>,
-  next?: Node<T>,
-}
+    value: T;
+    prev?: Node<T>;
+    next?: Node<T>;
+};
 
 export default class DoublyLinkedList<T> {
-  public length: number;
-  public head?: Node<T>;
+    public length: number;
+    public head?: Node<T>;
+    public tail?: Node<T>;
 
-  constructor() {
-    this.length = 0;
-  }
-
-  prepend(item: T): void {
-
-  }
-  insertAt(item: T, idx: number): void {
-
-  }
-  append(item: T): void {
-
-  }
-  remove(item: T): T | undefined {
-
-  }
-  get(idx: number): T | undefined {
-    let curr = this.head;
-    for (let i = 0; i < idx && curr; ++i) {
-      curr = curr.next;
+    constructor() {
+        this.length = 0;
+        this.head = undefined;
+        this.tail = undefined;
     }
 
-    return curr?.value;
-  }
-  removeAt(idx: number): T | undefined {
+    prepend(item: T): void {
+        const node = { value: item } as Node<T>;
 
-  }
+        this.length++;
+
+        if (!this.head) {
+            this.head = this.tail = node;
+            return;
+        }
+
+        node.next = this.head;
+        this.head.prev = node;
+        this.head = node;
+    }
+
+    insertAt(item: T, idx: number): void {
+        if (idx > this.length) {
+            throw new Error("oppsie");
+        }
+
+        if (idx === this.length) {
+            this.append(item);
+            return;
+        } else if (idx === 0) {
+            this.prepend(item);
+            return;
+        }
+
+        this.length++;
+        const curr = this.getAt(idx) as Node<T>;
+        const node = { value: item } as Node<T>;
+
+        node.next = curr;
+        node.prev = curr.prev;
+        curr.prev = node;
+
+        if (node.prev) {
+            node.prev.next = curr;
+        }
+    }
+    append(item: T): void {
+        this.length++;
+        const node = { value: item } as Node<T>;
+
+        if (!this.tail) {
+            this.head = this.tail = node;
+            return;
+        }
+        node.prev = this.tail;
+        this.tail.next = node;
+        this.tail = node;
+    }
+
+    remove(item: T): T | undefined {
+        let curr = this.head;
+
+        for (let i = 0; curr && i < this.length; ++i) {
+            if (curr.value === item) {
+                break;
+            }
+            curr = curr.next;
+        }
+
+        if (!curr) {
+            return undefined;
+        }
+
+        return this.removeNode(curr);
+    }
+
+    get(idx: number): T | undefined {
+        return this.getAt(idx)?.value;
+    }
+
+    removeAt(idx: number): T | undefined {
+        const node = this.getAt(idx);
+
+        if (!node) {
+            return undefined;
+        }
+
+        return this.removeNode(node);
+    }
+
+    private removeNode(node: Node<T>): T | undefined {
+        this.length--;
+
+        if (this.length === 0) {
+            const out = this.head?.value;
+            this.head = this.tail = undefined;
+            return out;
+        }
+
+        if (node.prev) {
+            node.prev.next = node.next;
+        }
+
+        if (node.next) {
+            node.next.prev = node.prev;
+        }
+
+        if (node === this.head) {
+            this.head = node.next;
+        }
+
+        if (node === this.tail) {
+            this.tail = node.prev;
+        }
+
+        node.prev = node.next = undefined;
+        return node.value;
+    }
+
+    private getAt(idx: number): Node<T> | undefined {
+        if (idx > this.length) {
+            throw new Error("oppsie");
+        }
+
+        if (idx === this.length) {
+            return this.tail;
+        } else if (idx === 0) {
+            return this.head;
+        }
+
+        let curr = this.head;
+        for (let i = 0; curr && i < idx; ++i) {
+            curr = curr.next;
+        }
+        return curr;
+    }
 }
